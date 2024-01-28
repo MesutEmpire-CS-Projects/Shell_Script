cca54efdc37e8ab9eb39b6b51c0bc57fa0bf2918
Samuel Wainaina <84708036+MesutEmpire@users.noreply.github.com>
2024-01-29 02:00:59 +0300

    Update question_7.sh

--- question_7.sh
+++ question_7.sh
@@ -7,7 +7,7 @@ SUBJECT="INVITATION TO THE 3RD YEAR PROJECTS WEBINAR"
 INVITATION_LETTER="Inviting the you as our computing student to our 3rd Year Projects on innovation incubation scheduled Monday 23rd October from 10:00 A.M.\n\nThe Zoom link for joining in on Friday is as given below.\n\nWe shall also broadcast the webinar on our youtube channel. Youtube link: https://youtu.be/ONVTA7LKMIs"
 
 #Senders Email Address
-FROM_EMAIL="mesut.empire00@gmail.com"
+FROM_EMAIL="john.doe@gmail.com"
 
 # Read the names and email addresses from the student's file
 while IFS=, read -r NAME TO_EMAIL
